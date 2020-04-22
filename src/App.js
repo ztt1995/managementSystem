6730b9b7747d3cc4a8003c6c5f7f69c1481f9cdb
ztt1995 <2414550385@qq.com>
2020-04-22 13:16:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* 
+    应用根组件
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{ Component } from "react";
+
+export default class App extends Component {
+
+    render(){
+        return(
+            <div>App</div>
+        )
+    }
 }
 
-export default App;
